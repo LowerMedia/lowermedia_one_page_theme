12b4b2aaf238fbe3ce7ae4e07dc89687788c1783
Pete Lower <pete.lower@gmail.com>
2013-05-27 12:20:53 -0500

    Adding functionality as class from codex

--- functions.php
+++ functions.php
@@ -149,35 +149,107 @@ add_action( 'wp_enqueue_scripts', 'lowermedia_one_page_theme_scripts' );
 
 
 /** Step 1. */
-function lowermedia_one_page_theme_menu() {
-	add_menu_page( 'One Page Theme Options', 'One Page Theme', 'manage_options', 'lowermedia-one-page-theme', 'lowermedia_one_page_theme_options' );
-}
+// function lowermedia_one_page_theme_menu() {
+// 	add_menu_page( 'One Page Theme Options', 'One Page Theme', 'manage_options', 'lowermedia-one-page-theme', 'lowermedia_one_page_theme_options' );
+// }
 /** Step 2 (from codex). */
-add_action( 'admin_menu', 'lowermedia_one_page_theme_menu' );
+//add_action( 'admin_menu', 'lowermedia_one_page_theme_menu' );
 
 /** Step 3. */
-function lowermedia_one_page_theme_options() {
-	if ( !current_user_can( 'manage_options' ) )  {
-		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
-	}
-	echo "<div class='wrap'>
-	<h2>LowerMedia One Page Theme Options</h2>
-	<p>
-	-Combine All Pages  OR<br/>
-	-Activate home page as splash page  OR<br/>
-	-Normal Setup
+// function lowermedia_one_page_theme_options() {
+// 	if ( !current_user_can( 'manage_options' ) )  {
+// 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+// 	}
+// 	echo "<div class='wrap'>
+// 	<h2>LowerMedia One Page Theme Options</h2>
+// 	<p>
+// 	-Combine All Pages  OR<br/>
+// 	-Activate home page as splash page  OR<br/>
+// 	-Normal Setup
 
-	<form method='post' action='options.php'> ";
-	settings_fields( 'lowermedia_one_page_theme_options' );
-	do_settings_fields( 'lowermedia_one_page_theme_options' );
-	submit_button();
-	echo "</form>
-	</p>
-	<p>
-	-upload background image?
-	</p>
-	</div>";
+// 	<form method='post' action='options.php'> ";
+// 	settings_fields( 'lowermedia_one_page_theme_options' );
+// 	do_settings_fields( 'lowermedia_one_page_theme_options' );
+// 	submit_button();
+// 	echo "</form>
+// 	</p>
+// 	<p>
+// 	-upload background image?
+// 	</p>
+// 	</div>";
+// }
+/*----------------------------*/
+class lowermedia_one_page_theme_admin_options{
+    public function __construct(){
+        if(is_admin()){
+	    add_action('admin_menu', array($this, 'lowermedia_one_page_theme_menu'));
+	    add_action('admin_init', array($this, 'page_init'));
+	}
+    }
+	
+    public function lowermedia_one_page_theme_menu(){
+        // This page will be under "Settings"
+	//add_options_page('Settings Admin', 'Settings', 'manage_options', 'test-setting-admin', array($this, 'create_admin_page'));
+	add_menu_page( 'One Page Theme Options', 'One Page Theme', 'manage_options', 'lowermedia-one-page-theme', array($this, 'create_admin_page') );
+    }
+
+    public function create_admin_page(){
+        ?>
+	<div class="wrap">
+	    <?php screen_icon(); ?>
+	    <h2>Settings</h2>			
+	    <form method="post" action="options.php">
+	        <?php
+                    // This prints out all hidden setting fields
+		    settings_fields('test_option_group');	
+		    do_settings_sections('test-setting-admin');
+		?>
+	        <?php submit_button(); ?>
+	    </form>
+	</div>
+	<?php
+    }
+	
+    public function page_init(){		
+	register_setting('test_option_group', 'array_key', array($this, 'check_ID'));
+		
+        add_settings_section(
+	    'setting_section_id',
+	    'Setting',
+	    array($this, 'print_section_info'),
+	    'test-setting-admin'
+	);	
+		
+	add_settings_field(
+	    'some_id', 
+	    'Some ID(Title)', 
+	    array($this, 'create_an_id_field'), 
+	    'test-setting-admin',
+	    'setting_section_id'			
+	);		
+    }
+	
+    public function check_ID($input){
+        if(is_numeric($input['some_id'])){
+	    $mid = $input['some_id'];			
+	    if(get_option('test_some_id') === FALSE){
+		add_option('test_some_id', $mid);
+	    }else{
+		update_option('test_some_id', $mid);
+	    }
+	}else{
+	    $mid = '';
+	}
+	return $mid;
+    }
+	
+    public function print_section_info(){
+	print 'Enter your setting below:';
+    }
+	
+    public function create_an_id_field(){
+        ?><input type="text" id="input_whatever_unique_id_I_want" name="array_key[some_id]" value="<?=get_option('test_some_id');?>" /><?php
+    }
 }
 
-
-
+$wctest = new wctest();
