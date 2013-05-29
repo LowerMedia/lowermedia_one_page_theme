be63c505f860a6ffda8245e9c0a1a7c085c4ed17
Pete Lower <pete.lower@gmail.com>
2013-05-29 15:10:49 -0500

    Uncommenting comment functionality

--- page.php
+++ page.php
@@ -36,17 +36,10 @@ get_header(); ?>
 					get_template_part( 'content', 'page' ); 
 				}
 				
-				
-
-				?>
-
-				<?php
-
-					//get_template_part( 'content', 'page' ); 
-
-					// If comments are open or we have at least one comment, load up the comment template
-					//if ( comments_open() || '0' != get_comments_number() )
-					//	comments_template();
+				// If comments are open or we have at least one comment, load up the comment template
+				if ( comments_open() || '0' != get_comments_number() )
+					comments_template();
+					
 				?>
 
 			<?php endwhile; // end of the loop. ?>
