218573b6aba5cb50b98a40b632a84323440aaf32
Pete Lower <pete.lower@gmail.com>
2013-07-23 17:37:35 -0500

    Not found page styles

--- no-results.php
+++ no-results.php
@@ -10,23 +10,23 @@
 
 <article id="post-0" class="post no-results not-found">
 	<header class="entry-header">
-		<h1 class="entry-title"><?php _e( 'Nothing Found', 'lowermedia_one_page_theme' ); ?></h1>
+		<h1 class="entry-title"><?php _e( 'LowerMedia One Page Theme', 'lowermedia_one_page_theme' ); ?></h1>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'lowermedia_one_page_theme' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+			<p><?php printf( __( 'Pick your homepage to make the One Page Theme work. <a href="wp-admin/options-reading.php">Pick here</a>.', 'lowermedia_one_page_theme' ) ); ?></p>
 
 		<?php elseif ( is_search() ) : ?>
 
 			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'lowermedia_one_page_theme' ); ?></p>
-			<?php get_search_form(); ?>
+			<?php //get_search_form(); ?>
 
 		<?php else : ?>
 
 			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'lowermedia_one_page_theme' ); ?></p>
-			<?php get_search_form(); ?>
+			<?php //get_search_form(); ?>
 
 		<?php endif; ?>
 	</div><!-- .entry-content -->
