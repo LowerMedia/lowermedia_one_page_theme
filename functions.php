4ed78c81f2cb2c090f7cecdad02770ac4566c5d5
Pete Lower <pete.lower@gmail.com>
2013-07-24 16:12:40 -0500

    Adding custom header functionality

--- functions.php
+++ functions.php
@@ -926,3 +926,16 @@ add_action( 'init', 'register_my_menus' );
 #   //
 */	
 add_theme_support( 'post-thumbnails', array( 'page' ) );          // Pages only
+/*############################################################################################
+#
+#   ADD CUSTOM HEADER IMAGE CAPABILITY
+#   //
+*/	
+$args = array(
+	'flex-width'    => true,
+	'width'         => 980,
+	'flex-height'    => true,
+	'height'        => 200,
+	'default-image' => get_template_directory_uri() . '/images/header.jpg',
+);
+add_theme_support( 'custom-header', $args );
--- page.php
+++ page.php
@@ -12,7 +12,6 @@
 
 get_header(); 
 
-
 ?>
 	
 	<div id="primary" class="content-area">
